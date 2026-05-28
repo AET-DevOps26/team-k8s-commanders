530d31f9bb418655c53feecadadcff6a4376f930
Florian Behrend <florian@livelyapps.com>
2026-05-28 11:35:36 +0200

    test(auth): run application tests against in-memory H2
    Replace Testcontainers Postgres with H2 for faster, lighter Spring Boot
    context tests that still validate application startup.

--- services/auth-service/src/test/java/com/caredesk/auth/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/caredesk/auth/AuthServiceApplicationTests.java
@@ -6,7 +6,12 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:tc:postgresql:16:///auth_db",
+        "spring.datasource.url=jdbc:h2:mem:auth_db;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
         "jwt.secret=test-secret-key-that-is-long-enough-for-hmac-256"
 })
 class AuthServiceApplicationTests {
